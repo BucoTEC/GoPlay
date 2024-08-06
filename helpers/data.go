50b9784088df8f9b9a51ddcbda1347e0a92f2f57
Adnan Bucalovic <adnan.bucalovic13@gmail.com>
2024-08-06 19:46:37 +0200

    setup struc methd

--- helpers/data.go
+++ helpers/data.go
@@ -1,7 +1,13 @@
 package helpers
 
+import "fmt"
+
 type Human struct {
 	Age int
 	FirstName string
 	LastName string
+}
+
+func (h Human) PrintFullName() {
+	fmt.Println(h.FirstName, h.LastName)
 }
