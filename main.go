10370e668e44238728ab2d98b2606f86a850219b
Adnan Bucalovic <adnan.bucalovic13@gmail.com>
2024-08-11 11:41:06 +0200

    add env reading

--- main.go
+++ main.go
@@ -1,8 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+
+	"github.com/joho/godotenv"
+)
 
 func main(){
+	godotenv.Load(".env")
+
+	port := os.Getenv("PORT")
 	name := "Adnan"
-	fmt.Printf("Hello my name is %v, nice to meet you",name)
+	fmt.Printf("Hello my name is %v, nice to meet you. I am running on port %v \n",name,port)
 }
