0205e3d578faefd9e02fc4e847d2ec5a5b4dfc44
Adnan Bucalovic <adnan.bucalovic13@gmail.com>
2024-08-15 15:02:41 +0200

    setup

--- go-orm/main.go
+++ go-orm/main.go
@@ -5,3 +5,36 @@ import "fmt"
 func main() {
 	fmt.Println("Hello from new server")
 }
+
+
+// 1. microservice AUTH GIN
+// login
+// register
+// get user by id
+// search user by email or username
+// update user
+// delete user
+// block user
+
+// api design
+// layered abroach
+// use vanilla sql
+
+
+
+// 2. microservice TRANSACTIONS FIBER
+// create wallet
+// update wallet
+// find wallet by id
+// search wallet by owner
+// make transactions
+// update transactions 15 min limit
+// DDD
+// sql db with GORM
+// make a generic repo
+
+
+// need to make a graphql
+// need to make grpc
+// need to make socket
+// mongo
