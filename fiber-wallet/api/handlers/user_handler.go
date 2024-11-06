529f31305b958e81a2579e5c7261afd05ff2ada5
Adnan Bucalovic <adnan.bucalovic13@gmail.com>
2024-11-07 00:16:09 +0100

    configure user routes

--- fiber-wallet/api/handlers/user_handler.go
+++ fiber-wallet/api/handlers/user_handler.go
@@ -1,1 +1,33 @@
 package handlers
+
+import "github.com/gofiber/fiber/v2"
+
+func SearchUsers() fiber.Handler {
+	return func(c *fiber.Ctx) error {
+		return c.SendString("search users")
+	}
+}
+
+func GetUser() fiber.Handler {
+	return func(c *fiber.Ctx) error {
+		return c.SendString("get user by id")
+	}
+}
+
+func CreateUser() fiber.Handler {
+	return func(c *fiber.Ctx) error {
+		return c.SendString("create user")
+	}
+}
+
+func UpdateUser() fiber.Handler {
+	return func(c *fiber.Ctx) error {
+		return c.SendString("update user")
+	}
+}
+
+func DeleteUser() fiber.Handler {
+	return func(c *fiber.Ctx) error {
+		return c.SendString("delete user")
+	}
+}
--- fiber-wallet/api/routes/user_routes.go
+++ fiber-wallet/api/routes/user_routes.go
@@ -1,10 +1,15 @@
 package routes
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/BucoTEC/fiber-wallet/api/handlers"
+	"github.com/gofiber/fiber/v2"
+)
 
 func UserRouter(app fiber.Router) {
 	userRoutes := app.Group("/users")
-	userRoutes.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Get all users")
-	})
+	userRoutes.Get("/", handlers.SearchUsers())
+	userRoutes.Get("/:id", handlers.GetUser())
+	userRoutes.Post("/", handlers.CreateUser())
+	userRoutes.Put("/:id", handlers.UpdateUser())
+	userRoutes.Delete("/:id", handlers.DeleteUser())
 }
