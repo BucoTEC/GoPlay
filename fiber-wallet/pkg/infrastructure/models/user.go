289f2162e4f4cd5b9d46ba49a5a8a7807c872213
Adnan Bucalovic <adnan.bucalovic13@gmail.com>
2024-11-15 14:26:18 +0100

    fix user model mapping

--- fiber-wallet/pkg/infrastructure/models/user.go
+++ fiber-wallet/pkg/infrastructure/models/user.go
@@ -6,8 +6,8 @@ import (
 
 type User struct {
 	gorm.Model
-	FirstName string    `gorm:"not null;" validate:"required"`
-	LastName  string    `gorm:"not null" validate:"required"`
+	FirstName string    `gorm:"not null;" validate:"required" json:"first_name"`
+	LastName  string    `gorm:"not null" validate:"required" json:"last_name"`
 	Email     string    `gorm:"not null;uniqueIndex;size:255" validate:"required,email"`
 	Password  string    `gorm:"not null;size:255" validate:"required,min=8"`
 	Wallets   []*Wallet `gorm:"many2many:user_wallets"`
