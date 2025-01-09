9a0b4226f0009604d62747b888b8130d860016a6
Adnan Bucalovic <adnan.bucalovic13@gmail.com>
2025-01-09 16:25:45 +0100

    add basic swagger

--- growth-go/main.go
+++ growth-go/main.go
@@ -1,9 +1,22 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	_ "growth-go/docs"
 
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/gofiber/swagger"
+)
+
+// @title			Order Api
+// @version		1.0
+// @description	This is an Order Api just for young people
+// @termsOfService	http://swagger.io/terms/
 func main() {
 	app := fiber.New()
+	app.Get("/swagger/*", swagger.HandlerDefault)
+
+	app.Use(cors.New())
 
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("Hello, World!")
