e2d4153afa140fd31f8d33da8ee70b326af3c667
Adnan Bucalovic <adnan.bucalovic13@gmail.com>
2025-02-15 16:46:55 +0100

    change to core main

--- main.go
+++ main.go
@@ -4,6 +4,25 @@ import "fmt"
 
 func main() {
 	fmt.Println("Hello World")
+
+	names := []string{"John", "Paul", "George", "Ringo"}
+
+	for i := 0; i < len(names); i++ {
+		fmt.Println(names[i])
+	}
+
+	type person struct {
+		Name string
+	}
+
+	adnan := person{Name: "Adnan"}
+
+	fmt.Println(adnan.Name)
+
+	names = append(names, "Adnan")
+
+	fmt.Println(names[len(names)-1])
+
 }
 
 // setup base loading
