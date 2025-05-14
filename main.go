5bdad2a5cfe6037ef5d48616605d44803f119ccb
Adnan Bucalovic <adnan.bucalovic13@gmail.com>
2025-05-14 14:38:57 +0200

    extreas

--- main.go
+++ main.go
@@ -1,5 +1,11 @@
 package main
 
+import "fmt"
+
 func main() {
-	// hello
+	fmt.Println("Hello, World!")
+	fmt.Println("This is a simple Go program.")
+	fmt.Println("It prints a greeting message to the console.")
+	fmt.Println("You can modify this code to add more functionality.")
+	fmt.Println("For example, you can add user input or perform calculations.")
 }
