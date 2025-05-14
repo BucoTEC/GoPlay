00775eeb9f79092abd4c9963023acebf11b914be
Adnan Bucalovic <adnan.bucalovic13@gmail.com>
2025-05-14 14:39:15 +0200

    clean up section one

--- main.go
+++ main.go
@@ -5,7 +5,5 @@ import "fmt"
 func main() {
 	fmt.Println("Hello, World!")
 	fmt.Println("This is a simple Go program.")
-	fmt.Println("It prints a greeting message to the console.")
-	fmt.Println("You can modify this code to add more functionality.")
-	fmt.Println("For example, you can add user input or perform calculations.")
+
 }
