2550fc4237daf5973cecabb9c2c9bb8831936f54
Adnan Bucalovic <adnan.bucalovic13@gmail.com>
2025-11-30 23:00:38 +0100

    pravim konflikt

--- testic.js
+++ testic.js
@@ -1,1 +1,19 @@
-//sarajla dodao
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+// Pravim konflikt namjerno
+rl.question('What is your name? ', (name) => {
+    console.log(`Hello, ${name}!`);
+    rl.close();
+});
+// ovo je  --- IGNORE ---
+// ovo je  --- IGNORE ---
+// ovo je  --- IGNORE ---
+// ovo je  --- IGNORE ---
+// ovo je  --- IGNORE ---
+// ovo je  --- IGNORE ---
+// ovo je  --- IGNORE ---
+// ovo je  --- IGNORE ---
